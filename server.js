590bac908cded0a4958bc397229b5897216cf20a
doop <doop>
2016-03-11 23:00:13 -1000

    Added create room stub

--- server.js
+++ server.js
@@ -11,4 +11,8 @@ var server = app.listen(8081, function () {
     var port = server.address().port;
     
     console.log("app listening on at http://%s%s", host,port);
-})
+});
+
+app.post('/api/create', function(res, req) {
+    console.log(req.body.text);
+});
