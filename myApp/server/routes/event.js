7c61247089343014cf1d179fa5e6fcf2a7170218
doop <doop>
2016-03-12 12:10:33 -1000

    added socket io support

--- myApp/server/routes/event.js
+++ myApp/server/routes/event.js
@@ -2,31 +2,31 @@ var express = require('express');
 var app = express();
 var router = express.Router();
 var bodyParser = require('body-parser');
-var db = require('./../../models');
-var Event = db.Event;
+//var db = require('./../../models');
+//var Event = db.Event;
 
 router.use(bodyParser.json());
 
 router.get('/',function(request, response){
-  Event.findAll()
+  /*Event.findAll()
     .then(function(data){
       response.render('events',{
         events : data
     });
-  });
+  });*/
 });
 
 router.get( '/:id', function( req, res) {
-  Event.findOne({
+  /*Event.findOne({
     where:{
       id: req.params.id
     }
   })
   .then (function (events){
     res.json( events );
-  });
+  });*/
 });
-
+/*
 router.post('/', function(req, res){
   console.log('event router', req.body);
   Event.create({
@@ -36,6 +36,30 @@ router.post('/', function(req, res){
   .then(function(data){
     return res.json(data);
   });
+});
+*/
+
+router.post('/shuffle_room', function(req, res) {
+   
+});
+
+router.post('/join_room', function(req, res) {
+   console.log('room joined', req.body);
+   return res.json({
+       
+   });
+});
+
+
+router.post('/create_room', function(req, res){
+  console.log('room created', req.body);
+  /*Event.create({
+    name : req.body.name,
+    passcode : req.body.passcode
+  })
+  .then(function(data){
+    return res.json(data);
+  });*/
 });
 
 module.exports = router;
--- myApp/server/server.js
+++ myApp/server/server.js
@@ -2,7 +2,9 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var CONFIG = require('./../config/config.json');
+var http = require('http').Server(app);
+
+//var CONFIG = require('./../config/config.json');
 var PORT = process.env.PORT || 3000;
 
 var session = require('express-session');
@@ -10,15 +12,38 @@ var session = require('express-session');
 var methodOverride = require('method-override');
 // var flash = require('connect-flash');
 // var bcrypt = require('bcrypt');
-var userId;
-var db = require('./../models');
-var Event = db.Event;
+//var userId;
+//var db = require('./../models');
+//var Event = db.Event;
 
-app.use(session(CONFIG.SESSION));
+//app.use(session(CONFIG.SESSION));
 
 app.use('/event', require('./routes/event.js'));
 
+app.use(express.static(__dirname + '/../www'));
+console.log(__dirname + '/../www');
+app.get('/', function (req, res) {
+   res.sendFile('index.html') ;
+});
+
 var server = app.listen(PORT, function(){
-  db.sequelize.sync();
-  process.stdout.write(`server listening on port ${PORT}`);
+  //db.sequelize.sync();
+  process.stdout.write('server listening on port ' + PORT);
+});
+
+var io = require('socket.io').listen(server);
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  socket.on('create', function (room) {
+        socket.join(room);
+        console.log(room + ' created');
+  });
+  socket.on('join', function (room) {
+      socket.join(room);
+      console.log(room + ' joined');
+  })
 });
