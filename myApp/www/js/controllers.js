567ccd0139c9152ba066e1da7096412b6c998ec1
RizOrt218 <rizhiafalen@gmail.com>
2016-03-12 15:54:51 -1000

    adds select time wip

--- myApp/www/js/controllers.js
+++ myApp/www/js/controllers.js
@@ -11,36 +11,40 @@ $ionicModal.fromTemplateUrl('adminView.html', {
     $scope.modal1 = modal;
   });
 
-  $ionicModal.fromTemplateUrl('guestView.html', {
-      id       : '2',
-      scope    : $scope,
-      animation: 'jelly'
-    }).then(function(modal) {
-      $scope.modal2 = modal;
-    });
+$ionicModal.fromTemplateUrl('guestView.html', {
+    id       : '2',
+    scope    : $scope,
+    animation: 'jelly'
+  }).then(function(modal) {
+    $scope.modal2 = modal;
+  });
 
-    $scope.openModal = function(index) {
-      switch (index) {
-        case 1 : $scope.modal1.show();
-                  break;
-        case 2 : $scope.modal2.show();
-      }
-    };
-    $scope.closeModal = function() {
-      $scope.modal1.hide();
-    };
-    //Cleanup the modal when we're done with it!
-    $scope.$on('$destroy', function() {
-      $scope.modal.remove();
-    });
-    // Execute action on hide modal
-    $scope.$on('modal.hidden', function() {
-      // Execute action
-    });
-    // Execute action on remove modal
-    $scope.$on('modal.removed', function() {
-      // Execute action
-    });
+  $scope.openModal = function(index) {
+    switch (index) {
+      case 1 : $scope.modal1.show();
+                break;
+      case 2 : $scope.modal2.show();
+    }
+  };
+  $scope.closeModal = function(index) {
+    switch (index) {
+      case 1 : $scope.modal1.hide();
+                break;
+      case 2 : $scope.modal2.hide();
+    }
+  };
+  //Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+  // Execute action on hide modal
+  $scope.$on('modal.hidden', function() {
+    // Execute action
+  });
+  // Execute action on remove modal
+  $scope.$on('modal.removed', function() {
+    // Execute action
+  });
 
 })
 
